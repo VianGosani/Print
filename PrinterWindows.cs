04f6a1411226b567ae8b5dd3580a73a09abb010e
Vian <howrain1@gmail.com>
2019-05-14 12:11:58 +0700

    Initial commit

---
+++ PrinterWindows.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print
+{
+    class PrinterWindows
+    {
+        public string Name { get; set; }
+
+        public virtual void Show()
+        {
+            Console.WriteLine();
+        }
+        public virtual void Print()
+        {
+            Console.WriteLine();
+        }
+    }
+}
